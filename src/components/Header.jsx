301852921c7cc8df2719eb46d8536d78a938b65a
parth3083 <parthbrajput30@gmail.com>
2024-03-04 23:34:14 +0530

    "Added the scroll-lock"

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,17 +4,29 @@ import { navigation } from '../constants'
 import { useLocation } from 'react-router-dom'
 import Button from './Button'
 import  MenuSvg  from '../assets/svg/MenuSvg'
-import {HamburgerMenu} from './design/Header'
+import { HamburgerMenu } from './design/Header'
+import {disablePageScroll, enablePageScroll} from 'scroll-lock'
 
 const Header = () => {
     const [openNavigation, setopenNavigation] = useState(true);
     const toogleNavigation = () => {
-        if (openNavigation)
+        if (openNavigation) {
+            disablePageScroll();
             setopenNavigation(false);
+        }
+            
         else
+        {
+            enablePageScroll();
             setopenNavigation(true);
+            }
+           
     }
     const handleClick = () => {
+        if (!openNavigation) {
+            return;
+        }
+        enablePageScroll();
         setopenNavigation(false);
     }
     const pathname = useLocation();
